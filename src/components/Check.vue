2dcd03b919d2f38325d108baaeb9bb93fdd9fcc4
simon-brd <brochard.simon85140@gmail.com>
2021-02-09 14:29:49 +0100

    Run build

---
+++ src/components/Check.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="check">
+    <h1>It looks ok ?</h1>
+  </div>
+</template>
+
---
+++ src/views/Test.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="test">
+    <Check></Check>
+  </div>
+</template>
+
+<script>
+
+import Check from '@/components/Check'
+
+export default {
+  name: 'Test',
+  components: {
+    Check
+  }
+
+}
+
+</script>
